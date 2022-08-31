689575f4ac3406be7e4fe378860941ef8b69b89c
Jaime Valasek <jaime@Jaime.localdomain>
2022-08-31 03:34:20 -0300

    instalação do sistema de authenticação do laravel

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::get('/notes', [NoteController::class, 'index'])->name('notes.index');
+
+require __DIR__.'/auth.php';
